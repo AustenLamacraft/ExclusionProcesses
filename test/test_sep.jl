868023acbdb308e614ee2bad38a71115e2b4d052
Austen Lamacraft <austen.lamacraft@gmail.com>
2018-01-24 12:49:13 +0000

    SEP Multispin test

--- test/test_sep.jl
+++ test/test_sep.jl
@@ -13,6 +13,15 @@ diff = xor.(init_spins, spin_state.spins)
 # For open boundary conditions the changed spins must be adjacent
 @test diff[indmax(diff) + 1] == true || sum(diff) == 0
 
-# Update of multispin configuration must cause convergence
+
+# Update of multispin configuration must cause convergence.
+# This means that each `UInt64` must change in a way that increases the absolute
+# value of the deviation from 32 of the number of 1s in its binary representation.
+score(ms::UInt64) = abs(count_ones(ms) - 32)
+ms_state = MultiSpinState(10,5)
+init_scores = [score(site) for site in ms_state.spins]
+update!(SEP, OpenBC, ms_state)
+final_scores = [score(site) for site in ms_state.spins]
+@test all(final_scores .>= init_scores)
 
 end # testset
